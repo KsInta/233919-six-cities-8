119dd20b774267ad833b249976bf7d6e09322310
DESKTOP-S207EEP\Vitalik <Ks.Inta@mail.ru>
2021-10-03 10:51:23 +0300

    Выполнил задание 'Начинаем программировать'

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,16 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+//import SignInScreen from '../sign-in-screen/sign-in-screen';
+//import FavoritesScreen from '../favorites-screen/favorites-screen';
+//import RoomScreen from '../room-screen/room-screen';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen offersCount={offersCount} />
+  );
 }
 
 export default App;
